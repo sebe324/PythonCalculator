e60e01abfeff804b41cb704716913b4dfdfaebb1
itsfelk <144376273+itsfelk@users.noreply.github.com>
2023-09-11 16:36:14 +0100

    Added Comments and cleaned code

--- src/python/misc.py
+++ src/python/misc.py
@@ -3,8 +3,11 @@ from playsound import playsound
 import threading
 import sys
 from layouts import *
+
+# Function to display an exit confirmation window
 def exit_confirmation(main_window):
-    exit_window = sg.Window("Do you want to exit?", [[sg.Button("Stay", **b_style2), sg.Button("Exit",**b_style3)]],enable_close_attempted_event=True)
+    exit_window = sg.Window("Do you want to exit?", [[sg.Button("Stay", **b_style2), sg.Button("Exit",**b_style3)]], enable_close_attempted_event=True)
+    
     while True:
         exit_event, exit_values = exit_window.read()
         if exit_event == "Exit":
@@ -15,13 +18,20 @@ def exit_confirmation(main_window):
             break
     exit_window.close()
 
+# Function to play a click sound on a separate thread
 def click_sound():
     t = threading.Thread(target=playsound, args=("./click.mp3",))
     t.start()
+
+# Function to insert a string into another string at a specified index
 def insert_str(string, str_to_insert, index):
     return string[:index] + str_to_insert + string[index:]
+
+# Function to remove a character from a string at a specified index
 def remove(s, indx):
-        return ''.join(x for x in s if s.index(x) != indx)
+    return ''.join(x for x in s if s.index(x) != indx)
+
+# Function to move a window to the center of the screen
 def move_center(window):
     screen_width, screen_height = window.get_screen_dimensions()
     win_width, win_height = window.size
